abd1435945298235846a2d01affba82867ff39aa
bossnelnel <65985277+bossnelnel@users.noreply.github.com>
2021-09-14 17:36:42 -0500

    Loop environment script
    environment loops

---
+++ BossHog/Assets/Scripts/MoveEnvironment.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnvironment : MonoBehaviour
+{
+    public float speed;
+    private float loopZ = 14;
+
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Scene's position at the start of the game
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move environment 
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+
+        // Loop when environment has reached designated Z coordinate
+        if (transform.position.z > loopZ)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
