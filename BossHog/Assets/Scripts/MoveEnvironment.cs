fb7cf97c44c6b82e971244addd9f2c4e2576ef2f
khietikko <kaiya.hietikko@gmail.com>
2021-09-15 14:02:30 -0500

    Environment model
    added the environment placeholder model and attached the env movement script

--- BossHog/Assets/Scripts/MoveEnvironment.cs
+++ BossHog/Assets/Scripts/MoveEnvironment.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveEnvironment : MonoBehaviour
 {
     public float speed;
-    private float loopZ = 14;
+    private float loopZ = 16;
 
     private Vector3 startPosition;
 
