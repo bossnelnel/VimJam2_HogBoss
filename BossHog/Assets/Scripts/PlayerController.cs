45b28e5da01a83e1060ee418b69f229ce15dac4f
0041 <zilinious@gmail.com>
2021-09-16 23:31:34 -0500

    Simplifying and Animating
    Simplied movement code
    
    Removed game border code (condensed into movement code)
    
    Moved player inputs into own section (no performance change, just more organized)
    
    Modified bullet reload script to continue reloading (even when fire button is not currently being held)
    
    Created empty object for motorcycle mesh components (more organized; allows for custom pivot point)
    
    Head turns to face direction of fire
    
    Motorcycle turns in direction of movement
    
    Motorcycle returns to neutral position when not moving

--- BossHog/Assets/Scripts/PlayerController.cs
+++ BossHog/Assets/Scripts/PlayerController.cs
@@ -16,6 +16,10 @@ public class PlayerController : MonoBehaviour
 
     public float HspeedVal;
 
+    public float maxTurn;
+    public float turnSpeed;
+    private float turnCurrent;
+
     private float rightBound = -10;
     private float leftBound = 10;
     private float bottomBound = 2;
@@ -25,84 +29,102 @@ public class PlayerController : MonoBehaviour
     private float loadTimer = 101;
 
     public GameObject bulletType;
+
+    private GameObject headMesh;
+    private GameObject motorcycleMesh;
     //private float rotationLimit = 30;
- 
 
-  
-
-    // Start is called before the first frame update
     void Start()
     {
-
+        headMesh = this.gameObject.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetChild(2).gameObject;
+        motorcycleMesh = this.gameObject.transform.GetChild(0).gameObject;
     }
 
     void FixedUpdate()
     {
-        #region Player movement
+        #region Player Inputs
+
         // Get the user's vertical input
-        verticalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         // Get the user's horizontal input
-        horizontalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
 
-        // Left/Right movement
-        transform.Translate(Vector3.right * speed * -horizontalInput * Time.deltaTime);
-        HspeedVal = speed * horizontalInput * Time.deltaTime;
-        //Debug.Log(HspeedVal);
-        // Forward/Back movement
-        transform.Translate(Vector3.forward * speed * verticalInput * Time.deltaTime);
+        // Get the user's horizontal fire input
+        horizFireInput = Input.GetAxis("Fire1");
+        // Get the user's vertical fire input
+        vertFireInput = Input.GetAxis("Fire2");
 
-        
         #endregion
 
-        #region Prevent player from leaving camera view
-        // Right wall
-        if (transform.position.x < rightBound)
+        #region Player Movement
+
+        //Left and Right movement on the 2D plane (clamps the new x-value between the left and right bounds of the map)
+        float x_pos = Mathf.Clamp(transform.position.x + (speed * -horizontalInput * Time.deltaTime), rightBound, leftBound);
+        //Up and down movement on the 2D plane (clamps the new z-value between the top and bottom bounds of the map)
+        float z_pos = Mathf.Clamp(transform.position.z + (speed * -verticalInput * Time.deltaTime), topBound, bottomBound);
+
+        //Creates a new Vec3 to define the player's new, updated position (a set position, not relative)
+        transform.position = new Vector3(x_pos, transform.position.y, z_pos);
+
+        #endregion
+
+        #region Animation Test
+
+        #region Head
+
+        float headRotationTarget = (-horizFireInput * 90.0f) + (45.0f * vertFireInput * Mathf.Sign(horizFireInput) * Mathf.Abs(horizFireInput));
+
+        headMesh.transform.rotation = Quaternion.Euler(0.0f, 180 + headRotationTarget, 0.0f);
+
+        #endregion
+
+        #region Motorcycle
+        // If the A or D key are being pressed, do the following
+        if (Mathf.Abs(horizontalInput) > 0)
         {
-            transform.position = new Vector3(rightBound, transform.position.y, transform.position.z);
+            // Add turnSpeed to turnCurrent (flip the sign if A), keep the value within the maxTurn value
+            turnCurrent = Mathf.Clamp(turnCurrent + (Mathf.Sign(horizontalInput) * turnSpeed), -maxTurn, maxTurn);
         }
-        // Left wall
-        if (transform.position.x > leftBound)
+        // If the A or D key are NOT being pressed AND the pig is turned 
+        else if(Mathf.Abs(turnCurrent) > 0)
         {
-            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
+            // Add (or subtract) half of turnSpeed from/to turnCurrent until it reaches 0 (neutral position)
+            turnCurrent += -Mathf.Sign(turnCurrent) * (turnSpeed / 2);
         }
-        // Bottom wall
-        if (transform.position.z > bottomBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, bottomBound);
-        }
-        // Top wall
-        if (transform.position.z < topBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y, topBound);
-        }
+        // Apply the turnCurrent value to the rotation of the player meshes to display
+        motorcycleMesh.transform.rotation = Quaternion.Euler(0.0f, 0.0f, turnCurrent);
+
+        #endregion
+
         #endregion
 
         #region Firing a bullet
-        horizFireInput = Input.GetAxis("Fire1");
-        vertFireInput = Input.GetAxis("Fire2");
 
-        if(horizFireInput != 0 || vertFireInput != 0)
+        if (loadTimer > reloadTime)
         {
-            if(loadTimer > reloadTime)
+            if (horizFireInput != 0 || vertFireInput != 0)
             {
                 Vector3 pos = transform.position;
                 Quaternion rot = Quaternion.identity;
-                if(Mathf.Abs(horizFireInput) == vertFireInput)
+                if (Mathf.Abs(horizFireInput) == vertFireInput)
                 {
                     rot = Quaternion.Euler(0.0f, 135.0f * horizFireInput, 0.0f);
-                } else if(Mathf.Abs(horizFireInput) == 1)
+                }
+                else if (Mathf.Abs(horizFireInput) == 1)
                 {
                     rot = Quaternion.Euler(0.0f, 90.0f * horizFireInput, 0.0f);
-                } else
+                }
+                else
                 {
                     rot = Quaternion.Euler(0.0f, 180.0f, 0.0f);
                 }
                 Instantiate(bulletType, pos, rot);
                 loadTimer = 0;
-            } else if(loadTimer <= reloadTime)
-            {
-                loadTimer++;
             }
+        }
+        else
+        {
+            loadTimer++;
         }
 
         /*
